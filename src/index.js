bc0bb59e786d3757ed8e895bec8107c12d2b15af
chawit033 <61050033@kmitl.ac.th>
2021-07-05 16:13:06 +0700

    read data

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import movieList from './movies.json'
+
+import './main.scss'
+
+document.addEventListener('DOMContentLoaded', function() {
+    const cards = document.getElementById('card')
+    const movies = movieList
+
+    console.log(movies)
+})
+
